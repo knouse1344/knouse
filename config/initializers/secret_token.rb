751f70157ee926df1b872b7d341eb4d6656062a1
Bryan Knouse <bryan.knouse@gmail.com>
2012-12-22 12:26:54 -0500

    first commit, seriously.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnouSe::Application.config.secret_token = '412a69f483efa0dd849ff17eeaa82bb3bdb3818782d181a7dc8ff5ca2c33526f8817a2801341423666d171adbdb9f0ec74b3d48cea11ca0e65fc8c086b67ab88'
