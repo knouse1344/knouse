0be620c3b2ebddd68d7583332199f1ec7f90dc37
Bryan Knouse <bryan.knouse@gmail.com>
2013-02-17 16:51:05 -0500

    gears are sweet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,73 @@ setInterval(function() {
         .css('-ms-transform', 'rotate('+angle+'deg)');
     angle=angle+.1;
 }, 50);
+
+var angle1 = 0.0;
+setInterval(function() {
+    console.log(angle1);
+    $(".gear_1")
+        .css('-webkit-transform', 'rotate('+angle1+'deg)')
+        .css('-moz-transform', 'rotate('+angle1+'deg)')
+        .css('-ms-transform', 'rotate('+angle1+'deg)');
+    angle1=angle1+.1;
+}, 25);
+
+var angle2 = 0.0;
+setInterval(function() {
+    console.log(angle2);
+    $(".gear_2")
+        .css('-webkit-transform', 'rotate('+angle2+'deg)')
+        .css('-moz-transform', 'rotate('+angle+'deg)')
+        .css('-ms-transform', 'rotate('+angle2+'deg)');
+    angle2=angle2-.1;
+}, 25);
+
+var angle3 = 0.0;
+setInterval(function() {
+    console.log(angle3);
+    $(".gear_3")
+        .css('-webkit-transform', 'rotate('+angle3+'deg)')
+        .css('-moz-transform', 'rotate('+angle3+'deg)')
+        .css('-ms-transform', 'rotate('+angle3+'deg)');
+    angle3=angle3+.1;
+}, 1);
+
+var angle4 = 0.0;
+setInterval(function() {
+    console.log(angle4);
+    $(".gear_4")
+        .css('-webkit-transform', 'rotate('+angle4+'deg)')
+        .css('-moz-transform', 'rotate('+angle4+'deg)')
+        .css('-ms-transform', 'rotate('+angle4+'deg)');
+    angle4=angle4+.1;
+}, 1);
+
+var angle5 = 0.0;
+setInterval(function() {
+    console.log(angle5);
+    $(".gear_5")
+        .css('-webkit-transform', 'rotate('+angle5+'deg)')
+        .css('-moz-transform', 'rotate('+angle5+'deg)')
+        .css('-ms-transform', 'rotate('+angle5+'deg)');
+    angle5=angle5-.1;
+}, 1);
+
+var angle6 = 0.0;
+setInterval(function() {
+    console.log(angle6);
+    $(".gear_6")
+        .css('-webkit-transform', 'rotate('+angle6+'deg)')
+        .css('-moz-transform', 'rotate('+angle6+'deg)')
+        .css('-ms-transform', 'rotate('+angle6+'deg)');
+    angle6=angle6-1;
+}, 1);
+
+var angle7 = 0.0;
+setInterval(function() {
+    console.log(angle7);
+    $(".gear_7")
+        .css('-webkit-transform', 'rotate('+angle7+'deg)')
+        .css('-moz-transform', 'rotate('+angle7+'deg)')
+        .css('-ms-transform', 'rotate('+angle7+'deg)');
+    angle7=angle7+1;
+}, 1);
