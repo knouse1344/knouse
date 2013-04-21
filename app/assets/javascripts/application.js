fb5653d4feb00ddd40f9f6ddd56fc8ce8da3f1ab
Bryan Knouse <bryan.knouse@gmail.com>
2013-04-21 18:28:17 -0400

    GEARS everywhere

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,83 +15,92 @@
 //= require_tree .
 
 
+// var angle1 = 0.0;
+// setInterval(function() {
+//     console.log(angle1);
+//     $(".gear_1")
+//         .css('-webkit-transform', 'rotate('+angle1+'deg)')
+//         .css('-moz-transform', 'rotate('+angle1+'deg)')
+//         .css('-ms-transform', 'rotate('+angle1+'deg)');
+//     angle1=angle1+.1;
+// }, 25);
 
-var angle = 0.0;
-setInterval(function() {
-    console.log(angle);
-    $("#clownpenis")
-        .css('-webkit-transform', 'rotate('+angle+'deg)')
-        .css('-moz-transform', 'rotate('+angle+'deg)')
-        .css('-ms-transform', 'rotate('+angle+'deg)');
-    angle=angle+.1;
-}, 50);
+// var angle2 = 0.0;
+// setInterval(function() {
+//     console.log(angle2);
+//     $(".gear_2")
+//         .css('-webkit-transform', 'rotate('+angle2+'deg)')
+//         .css('-moz-transform', 'rotate('+angle+'deg)')
+//         .css('-ms-transform', 'rotate('+angle2+'deg)');
+//     angle2=angle2-.1;
+// }, 25);
 
-var angle1 = 0.0;
-setInterval(function() {
-    console.log(angle1);
-    $(".gear_1")
-        .css('-webkit-transform', 'rotate('+angle1+'deg)')
-        .css('-moz-transform', 'rotate('+angle1+'deg)')
-        .css('-ms-transform', 'rotate('+angle1+'deg)');
-    angle1=angle1+.1;
-}, 25);
+// var angle3 = 0.0;
+// setInterval(function() {
+//     console.log(angle3);
+//     $(".gear_3")
+//         .css('-webkit-transform', 'rotate('+angle3+'deg)')
+//         .css('-moz-transform', 'rotate('+angle3+'deg)')
+//         .css('-ms-transform', 'rotate('+angle3+'deg)');
+//     angle3=angle3+.1;
+// }, 1);
 
-var angle2 = 0.0;
-setInterval(function() {
-    console.log(angle2);
-    $(".gear_2")
-        .css('-webkit-transform', 'rotate('+angle2+'deg)')
-        .css('-moz-transform', 'rotate('+angle+'deg)')
-        .css('-ms-transform', 'rotate('+angle2+'deg)');
-    angle2=angle2-.1;
-}, 25);
+// var angle4 = 0.0;
+// setInterval(function() {
+//     console.log(angle4);
+//     $(".gear_4")
+//         .css('-webkit-transform', 'rotate('+angle4+'deg)')
+//         .css('-moz-transform', 'rotate('+angle4+'deg)')
+//         .css('-ms-transform', 'rotate('+angle4+'deg)');
+//     angle4=angle4+.1;
+// }, 1);
 
-var angle3 = 0.0;
+// var angle5 = 0.0;
+// setInterval(function() {
+//     console.log(angle5);
+//     $(".gear_5")
+//         .css('-webkit-transform', 'rotate('+angle5+'deg)')
+//         .css('-moz-transform', 'rotate('+angle5+'deg)')
+//         .css('-ms-transform', 'rotate('+angle5+'deg)');
+//     angle5=angle5-.1;
+// }, 1);
+
+// var angle6 = 0.0;
+// setInterval(function() {
+//     console.log(angle6);
+//     $(".gear_6")
+//         .css('-webkit-transform', 'rotate('+angle6+'deg)')
+//         .css('-moz-transform', 'rotate('+angle6+'deg)')
+//         .css('-ms-transform', 'rotate('+angle6+'deg)');
+//     angle6=angle6-1;
+// }, 1);
+
+var anglea = 0.0;
 setInterval(function() {
-    console.log(angle3);
-    $(".gear_3")
-        .css('-webkit-transform', 'rotate('+angle3+'deg)')
-        .css('-moz-transform', 'rotate('+angle3+'deg)')
-        .css('-ms-transform', 'rotate('+angle3+'deg)');
-    angle3=angle3+.1;
+    console.log(anglea);
+    $(".gear_a")
+        .css('-webkit-transform', 'rotate('+anglea+'deg)')
+        .css('-moz-transform', 'rotate('+anglea+'deg)')
+        .css('-ms-transform', 'rotate('+anglea+'deg)');
+    anglea=anglea+.1;
 }, 1);
 
-var angle4 = 0.0;
+var angleb = 0.0;
 setInterval(function() {
-    console.log(angle4);
-    $(".gear_4")
-        .css('-webkit-transform', 'rotate('+angle4+'deg)')
-        .css('-moz-transform', 'rotate('+angle4+'deg)')
-        .css('-ms-transform', 'rotate('+angle4+'deg)');
-    angle4=angle4+.1;
+    console.log(angleb);
+    $(".gear_b")
+        .css('-webkit-transform', 'rotate('+angleb+'deg)')
+        .css('-moz-transform', 'rotate('+angleb+'deg)')
+        .css('-ms-transform', 'rotate('+angleb+'deg)');
+    angleb=angleb-.1;
 }, 1);
 
-var angle5 = 0.0;
+var anglec = 60.0;
 setInterval(function() {
-    console.log(angle5);
-    $(".gear_5")
-        .css('-webkit-transform', 'rotate('+angle5+'deg)')
-        .css('-moz-transform', 'rotate('+angle5+'deg)')
-        .css('-ms-transform', 'rotate('+angle5+'deg)');
-    angle5=angle5-.1;
-}, 1);
-
-var angle6 = 0.0;
-setInterval(function() {
-    console.log(angle6);
-    $(".gear_6")
-        .css('-webkit-transform', 'rotate('+angle6+'deg)')
-        .css('-moz-transform', 'rotate('+angle6+'deg)')
-        .css('-ms-transform', 'rotate('+angle6+'deg)');
-    angle6=angle6-1;
-}, 1);
-
-var angle7 = 0.0;
-setInterval(function() {
-    console.log(angle7);
-    $(".gear_7")
-        .css('-webkit-transform', 'rotate('+angle7+'deg)')
-        .css('-moz-transform', 'rotate('+angle7+'deg)')
-        .css('-ms-transform', 'rotate('+angle7+'deg)');
-    angle7=angle7+1;
+    console.log(anglec);
+    $(".gear_c")
+        .css('-webkit-transform', 'rotate('+anglec+'deg)')
+        .css('-moz-transform', 'rotate('+anglec+'deg)')
+        .css('-ms-transform', 'rotate('+anglec+'deg)');
+    anglec=anglec+.1;
 }, 1);
